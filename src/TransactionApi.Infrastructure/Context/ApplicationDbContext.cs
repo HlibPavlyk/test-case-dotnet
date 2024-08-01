fe8d8f69d34d68d0b2d0de458d54b8602510785e
HlibPavlyk <pavlikgo78@gmail.com>
2024-08-01 12:38:13 +0300

    feat: prepare for migration
    - add necessary packages
    - add db context
    - set entity configuration

---
+++ src/TransactionApi.Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TransactionApi.Domain.Entities;
+
+namespace TransactionApi.Infrastructure.Context;
+
+public class ApplicationDbContext : DbContext
+{
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<Transaction> Transactions { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
+
+}
---
+++ src/TransactionApi.Infrastructure/EntityTypeConfigurations/TransactionConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TransactionApi.Domain.Entities;
+
+namespace TransactionApi.Infrastructure.EntityTypeConfigurations;
+
+public class TransactionConfiguration : IEntityTypeConfiguration<Transaction>
+{
+    public void Configure(EntityTypeBuilder<Transaction> builder)
+    {
+        builder.HasKey(t => t.TransactionId);
+        
+        builder.Property(t => t.TransactionId)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(t => t.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(t => t.Email)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(t => t.Amount)
+            .IsRequired()
+            .HasColumnType("decimal(18,2)");
+
+        builder.Property(t => t.TransactionDate)
+            .IsRequired();
+
+        builder.Property(t => t.ClientLocation)
+            .IsRequired()
+            .HasMaxLength(100);
+    }
+}
