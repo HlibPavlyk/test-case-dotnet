1b2eed01ef23812b607bf8f52aead2f5da53a3a9
HlibPavlyk <pavlikgo78@gmail.com>
2024-08-02 14:46:50 +0300

    feat: add transaction controller
    - add api base controller

---
+++ src/TransactionApi.Presentation/Controllers/TransactionController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TransactionApi.Application.Transactions.Commands.ImportTransactions;
+
+namespace TransactionApi.Presentation.Controllers;
+
+[Route("api/transactions")]
+public class TransactionController : ApiController
+{
+    public TransactionController(ISender sender) : base(sender) { }
+    
+    [HttpPost("import")]
+    public async Task<IActionResult> ImportTransactionsAsync([FromForm] ImportTransactionCommand command)
+    {
+        try
+        {
+            var result = await Sender.Send(command);
+            return Ok(result);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+}
