ad46b1e635d92d4029f1fc74fc3dcd6d1c13d248
HlibPavlyk <pavlikgo78@gmail.com>
2024-08-02 14:46:09 +0300

    feat: add import transaction command and handler

---
+++ src/TransactionApi.Application/Transactions/Commands/ImportTransactions/ImportTransactionCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+
+namespace TransactionApi.Application.Transactions.Commands.ImportTransactions;
+
+public sealed record ImportTransactionCommand(IFormFile File) : IRequest;
---
+++ src/TransactionApi.Application/Transactions/Commands/ImportTransactions/ImportTransactionCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using TransactionApi.Application.Interfaces;
+
+namespace TransactionApi.Application.Transactions.Commands.ImportTransactions;
+
+public sealed class ImportTransactionCommandHandler : IRequestHandler<ImportTransactionCommand>
+{
+    private readonly IFileDataAccess _fileDataAccess;
+
+    public ImportTransactionCommandHandler(IFileDataAccess fileDataAccess)
+    {
+        _fileDataAccess = fileDataAccess;
+    }
+
+    public async Task<Unit> Handle(ImportTransactionCommand request, CancellationToken cancellationToken)
+    {
+        var transactions = await _fileDataAccess.ReadTransactionsFromFileAsync(request.File);
+        
+        return Unit.Value;
+
+    }
+}
