e9faf270eb1fb5c7f4b69822804b74f18603f120
HlibPavlyk <pavlikgo78@gmail.com>
2024-08-03 01:17:33 +0300

    feat: add export excel controller
    - add cancellation tokens to endpoints

--- src/TransactionApi.Presentation/Controllers/TransactionController.cs
+++ src/TransactionApi.Presentation/Controllers/TransactionController.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using TransactionApi.Application.Transactions.Commands.ImportTransactions;
+using TransactionApi.Application.Transactions.Queries.ExportTransactions;
 
 namespace TransactionApi.Presentation.Controllers;
 
@@ -9,13 +10,28 @@ public class TransactionController : ApiController
 {
     public TransactionController(ISender sender) : base(sender) { }
     
-    [HttpPost("import")]
-    public async Task<IActionResult> ImportTransactionsAsync([FromForm] ImportTransactionCommand command)
+    [HttpPost("import-csv")]
+    public async Task<IActionResult> ImportTransactionsAsync([FromForm] ImportTransactionCommand command,
+        CancellationToken cancellationToken)
     {
         try
         {
-            var result = await Sender.Send(command);
+            var result = await Sender.Send(command, cancellationToken);
             return Ok(result);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+    
+    [HttpGet("export-excel")]
+    public async Task<IActionResult> ExportTransactionsAsync(CancellationToken cancellationToken)
+    {
+        try
+        {
+            var result = await Sender.Send(new ExportTransactionsQuery(), cancellationToken);
+            return File(result, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "transactions.xlsx");
         }
         catch (Exception e)
         {
