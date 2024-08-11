feb1d2204c882d66c3f3e43b143754c82cb19475
HlibPavlyk <pavlikgo78@gmail.com>
2024-08-11 21:30:56 +0300

    feat: add other get methods in controller
    - make some changes in .csproj files
    - edit startup file

--- src/TransactionApi.Presentation/Controllers/TransactionController.cs
+++ src/TransactionApi.Presentation/Controllers/TransactionController.cs
@@ -1,7 +1,14 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TransactionApi.Application.Dtos;
 using TransactionApi.Application.Transactions.Commands.ImportTransactions;
 using TransactionApi.Application.Transactions.Queries.ExportTransactions;
+using TransactionApi.Application.Transactions.Queries.GetTransactionsByClientLocalDates;
+using TransactionApi.Application.Transactions.Queries.GetTransactionsByDatesForCurrentClientTimeZone;
+using TransactionApi.Application.Transactions.Queries.GetTransactionsInJanuary2024;
+using TransactionApi.Domain.Entities;
+using TransactionApi.Domain.Exceptions;
 
 namespace TransactionApi.Presentation.Controllers;
 
@@ -9,7 +16,7 @@ namespace TransactionApi.Presentation.Controllers;
 public class TransactionController : ApiController
 {
     public TransactionController(ISender sender) : base(sender) { }
-    
+
     [HttpPost("import-csv")]
     public async Task<IActionResult> ImportTransactionsAsync([FromForm] ImportTransactionCommand command,
         CancellationToken cancellationToken)
@@ -24,7 +31,7 @@ public class TransactionController : ApiController
             return BadRequest(e.Message);
         }
     }
-    
+
     [HttpGet("export-excel")]
     public async Task<IActionResult> ExportTransactionsAsync(CancellationToken cancellationToken)
     {
@@ -32,6 +39,69 @@ public class TransactionController : ApiController
         {
             var result = await Sender.Send(new ExportTransactionsQuery(), cancellationToken);
             return File(result, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "transactions.xlsx");
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpGet("get-by-local-dates")]
+    public async Task<IActionResult> GetAllTransactionsByLocalDatesAsync(
+        [FromQuery] GetTransactionsByClientLocalDatesQuery query, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var result = await Sender.Send(query, cancellationToken);
+            return Ok(result);
+        }
+        catch (NotFoundException e)
+        {
+            return NotFound(e.Message);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpGet("get-by-dates-for-current-time-zone")]
+    public async Task<IActionResult> GetTransactionsByDatesForCurrentClientTimeZoneQuery (
+        [FromQuery] DateTime startDate, 
+        [FromQuery] DateTime endDate, 
+        [FromQuery] int page, 
+        [FromQuery] int pageSize,
+        [FromHeader(Name = "X-Timezone")] string timezone, 
+        CancellationToken cancellationToken)
+    {
+        try
+        {
+            var result = await Sender.Send(new GetTransactionsByDatesForCurrentClientTimeZoneQuery(startDate, endDate,
+                timezone, page, pageSize), cancellationToken);
+            return Ok(result);
+        }
+        catch (NotFoundException e)
+        {
+            return NotFound(e.Message);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpGet("get-in-january-2024")]
+    public async Task<IActionResult> GetTransactionsInJanuary2024Query(
+        [FromQuery] int page, [FromQuery] int pageSize, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var result = await Sender.Send(new GetTransactionsInJanuary2024Query(page, pageSize), cancellationToken);
+            return Ok(result);
+        }
+        catch (NotFoundException e)
+        {
+            return NotFound(e.Message);
         }
         catch (Exception e)
         {
--- src/TransactionApi.Web/Program.cs
+++ src/TransactionApi.Web/Program.cs
@@ -1,14 +1,19 @@
-using System.Data;
+using System.Reflection;
 using MediatR;
-using Microsoft.Data.SqlClient;
-using TransactionApi.Application.Abstractions;
-using TransactionApi.Infrastructure.DataAccess;
+using Microsoft.OpenApi.Models;
 using TransactionApi.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Transaction API", Version = "v1" });
+
+    var xmlFile = @"..\..\..\..\TransactionApi.Presentation\bin\Debug\net8.0\TransactionWeb.Presentation.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 
 builder
@@ -25,7 +30,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Transaction API V1");
+    });
 }
 
 app.UseHttpsRedirection();
